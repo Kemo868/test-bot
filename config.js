902425c327e58e383cf8867358d25d4a21a2356c
7cc8 <135761741+7cc8@users.noreply.github.com>
2024-01-21 08:36:11 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-{
-    "token" : "MTE5MjU4OTczNzk5MTYxMDQyOA.GFFTa0.R0SDVjWxj_eviO8IBqRw_2nRMjk1WbnajIRQCc",
-    "prefix": "-",
-    "clientId": "1192589737991610428",
-    "guildId": "1192590295137792091"
-}
